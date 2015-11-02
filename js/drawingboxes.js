3dbdd51c1f11cc70a05facd061971567d4786e61
iddy <lemon.and.lime@hotmail.com>
2015-11-03 01:33:13 +1300

    ui and json work separately, next step is joinging them -- ui to json to render

--- js/drawingboxes.js
+++ js/drawingboxes.js
@@ -3,49 +3,128 @@ var draw_panel = {
   front: '',
 }
 
+/* initialises the drawing panel) */
 function init_panel() {
   draw_panel.top = d3.select("#top_view")
       .append("svg")
         .attr('id', 'svg_top')
         .attr("width", 200)
-        .attr("height", 200)
-      .append("circle")
-        .attr('class', 'draggable')
-        .attr('onmousedown',"selectElement(evt)")
-        .attr('transform',"matrix(1 0 0 1 0 0)")
-        .attr("cx", 25)
-        .attr("cy", 25)
-        .attr("r", 25)
-        .style("fill", "purple");
-  draw_panel.front = d3.select("#front_view")
-      .append("svg")
-        .attr('id', 'svg_front')
-        .attr("width", 200)
-        .attr("height", 200)
-      .append("circle")
-        .attr('class', 'draggable')
+        .attr("height", 200);
+        
+  draw_panel.top.append("circle")
+        .attr('class', 'draggable top_circle')
+        .attr('id', 'sphere_top_1')
         .attr('onmousedown',"selectElement(evt)")
         .attr('transform',"matrix(1 0 0 1 0 0)")
         .attr("cx", 50)
         .attr("cy", 50)
         .attr("r", 25)
-        .style("fill", "purple");
+        .style("fill", "#800080");
+        
+  draw_panel.front = d3.select("#front_view")
+      .append("svg")
+        .attr('id', 'svg_front')
+        .attr("width", 200)
+        .attr("height", 200);
+        
+  draw_panel.front.append("circle")
+        .attr('class', 'draggable front_circle')
+        .attr('id', 'sphere_front_1')
+        .attr('onmousedown',"selectElement(evt)")
+        .attr('transform',"matrix(1 0 0 1 0 0)")
+        .attr("cx", 50)
+        .attr("cy", 50)
+        .attr("r", 25)
+        .style("fill", "#800080");
+}
+
+/* Adds a sphere to the drawing panel */
+var circleCount = 1; // id number of the last circle created (not really a 'count')
+function addCircle() {
+  circleCount++;
+  draw_panel.top.append("circle")
+        .attr('class', 'draggable top_circle')
+        .attr('id', 'sphere_top_'+circleCount)
+        .attr('onmousedown',"selectElement(evt)")
+        .attr('transform',"matrix(1 0 0 1 0 0)")
+        .attr("cx", 100)
+        .attr("cy", 100)
+        .attr("r", 25)
+        .style("fill", "#800080");
+  draw_panel.front.append("circle")
+        .attr('class', 'draggable front_circle')
+        .attr('id', 'sphere_front_'+circleCount)
+        .attr('onmousedown',"selectElement(evt)")
+        .attr('transform',"matrix(1 0 0 1 0 0)")
+        .attr("cx", 100)
+        .attr("cy", 100)
+        .attr("r", 25)
+        .style("fill", "#800080");
+}
+
+/* Removes the last selected circle from the drawing area */
+function remCircle() {
+  if(selectedElement != 0){
+    selectedElement.remove();
+    selectedPartner.remove();
+    
+    selectedElement = 0;
+    selectedPartner = 0;
+  }
+}
+
+/* Sets the colour of the last selected sphere */
+function colCircle(col) {
+  if(selectedElement != 0){
+    selectedElement.style.fill = col;
+    selectedPartner.style.fill = col;
+  }
+}
+
+/* Sets the radius of the last selected sphere */
+function setRadius(radius) {
+  if(selectedElement != 0){
+    selectedElement.setAttribute('r',radius);
+    selectedPartner.setAttribute('r',radius);
+  }
 }
 
 var selectedElement = 0;
+var selectedPartner = 0;
 var currentX = 0;
 var currentY = 0;
 var currentMatrix = 0;
+var partnerMatrix = 0;
+
+/* Selects the given ID's corresponding circle */
+function getPartner(selectedId) {
+  var sel_id = selectedElement.getAttribute('id');
+  var partner_id = '';
+  var id_num = sel_id.match(new RegExp('\\d+'));
+  
+  if (sel_id.match('top')) {
+    partner_id = 'sphere_front_'+id_num;
+  }
+  else if (sel_id.match('front')) {
+    partner_id = 'sphere_top_'+id_num;
+  }
+  return document.getElementById(partner_id)
+}
 
 function selectElement(evt) {
+  // select clicked and partner circles
   selectedElement = evt.target;
+  selectedPartner = getPartner(selectedElement);
+  
   currentX = evt.clientX;
   currentY = evt.clientY;
   currentMatrix = selectedElement.getAttributeNS(null, "transform").slice(7,-1).split(' ');
+  partnerMatrix = selectedPartner.getAttributeNS(null, "transform").slice(7,-1).split(' ');
    
-    for(var i=0; i<currentMatrix.length; i++) {
-      currentMatrix[i] = parseFloat(currentMatrix[i]);
-    }
+  for(var i=0; i<currentMatrix.length; i++) {
+    currentMatrix[i] = parseFloat(currentMatrix[i]);
+    partnerMatrix[i] = parseFloat(partnerMatrix[i]);
+  }
 
   selectedElement.setAttributeNS(null, "onmousemove", "moveElement(evt)");
   selectedElement.setAttributeNS(null, "onmouseout", "deselectElement(evt)");
@@ -57,13 +136,17 @@ function moveElement(evt){
   dy = evt.clientY - currentY;
   currentMatrix[4] += dx;
   currentMatrix[5] += dy;
+  partnerMatrix[4] += dx;
   newMatrix = "matrix(" + currentMatrix.join(' ') + ")";
+  newPartMatrix = "matrix(" + partnerMatrix.join(' ') + ")";
             
   selectedElement.setAttributeNS(null, "transform", newMatrix);
+  selectedPartner.setAttributeNS(null, "transform", newPartMatrix);
   currentX = evt.clientX;
   currentY = evt.clientY;
   
-  console.log(currentX+" "+currentY);
+  
+  // console.log(currentX+" "+currentY);
 }
 
 function deselectElement(evt){
@@ -71,7 +154,7 @@ function deselectElement(evt){
     selectedElement.removeAttributeNS(null, "onmousemove");
     selectedElement.removeAttributeNS(null, "onmouseout");
     selectedElement.removeAttributeNS(null, "onmouseup");
-    selectedElement = 0;
+    // selectedElement = 0;
   }
 }
 
--- js/putimage.js
+++ js/putimage.js
@@ -1,42 +1,221 @@
+/** Create the canvas for rendering
+ *    Build PNG pixel array
+ *    Create DOM element to hold it
+ */
 var getCanvas = function(wd,ht) {
     var p = new PNGlib(wd, ht, 256);      // constructor takes height, weight and colour-depth
     var background = p.color(40, 40, 40, 0); // set the background transparent
-    // initCanvas(p, wd, ht); // set pixels so can display
-    
-    // var svg = d3.select("#theimage")
-      // .append("svg")
-      // .attr("width",wd)
-      // .attr("height",ht);
-    // var image = svg.selectAll("image");
-    // image.append("svg:image")
-      // .attr("width",wd)
-      // .attr("height",ht)
-      // .attr("xlink/href",'<img id="img_canvas" src="data:image/png;base64,'+p.getBase64()+'">');
     $('#theimage').append('<img id="img_canvas" src="data:image/png;base64,'+p.getBase64()+'">');
     return p;
 }
 
-var pixelss = []
+/* Perform gamma correction and impose maximum colour value of 255 */
+GAMMA_CORRECTION = 1.0/3.2 // was 2.2
+var processPixel = function(color) {
+    var col = Math.floor( Math.pow( color/255.0 , GAMMA_CORRECTION) * 255);
+    // console.log(color+'->'+col);
+    return col>255?255:col;
+}
+
+/* Processes new data, updating the image canvas */
+var pixelss = []; // used for debugging
 var drawPixels = function(canvas, pixels) {
     pixelss.push(JSON.stringify(pixels)+'\n');
     for (var i = 0; i < pixels.length; i++) {
         var pix = pixels[i]
         canvas.buffer[canvas.index(pix.x, pix.y)] = canvas.color(
-            pix.col[0]>255?255:pix.col[0],
-            pix.col[1]>255?255:pix.col[1], 
-            pix.col[2]>255?255:pix.col[2]);
+            processPixel(pix.col[0]),
+            processPixel(pix.col[1]), 
+            processPixel(pix.col[2]));
     }
     $('#img_canvas').remove();
     $('#theimage').append('<img id="img_canvas" src="data:image/png;base64,'+canvas.getBase64()+'">');
 }
 
+var renderData = {
+  'section':[], 
+  'lightSource': {'x':-10,'y':0,'z':0},
+  'cameraPos': {'x':0,'y':0,'z':40},
+  'objects': [
+    { 'type':'sphere',
+      'center': {'x':0,'y':0,'z':0},
+      'radius': 5.0,
+      'colour': {'x':0,'y':255,'z':0}
+    },
+    { 'type':'plane',
+      'point': {'x':0,'y':0,'z':-12},
+      'normal': {'x':0,'y':0,'z':1},
+      'colour': {'x':255,'y':255,'z':255}
+    }
+  ],
+}
+
+/* Dimensions of the render's canvas */
+DIMENSIONS = {
+  width: 400,
+  height: 400,
+  div_x: 30,
+  div_y: 30
+}
+
+/* Initialise the display */
+$(document).ready(function() {
+  init_panel();
+  canvas = getCanvas(DIMENSIONS.width, DIMENSIONS.height);
+});
+
+/** Starts the rendering
+ *    Divides canvas into sections
+ *    Initiates sending the sections
+ */
+function startCanvasRender() {
+  // canvas = getCanvas(width,height);
+  // testFullCanvas(canvas, width, height);
+  
+  var s = getSections(DIMENSIONS.width, DIMENSIONS.height, DIMENSIONS.div_x, DIMENSIONS.div_y);
+  
+  /* remove some sections, used for testing */
+  // var limit = Math.floor(s.length * 0.4);
+  // for (var i = 0; i < limit; i++) s.shift(); // from front
+  // for (var i = 0; i < limit; i++) s.pop();   // from back
+  /* </remove> */
+  
+  sendingSections.sendSections(renderData,s);
+}
+
+/* Update the image with new data from the server */
+var numDraws = 0;
+function updateImage(data) {
+  numDraws++;
+  drawPixels(canvas, data);
+}
+
+/** Divides the image up into sections to be rendered
+ *    Dimensions of the final image
+ *    Max dimensions of the sections to be sent
+ */
+function getSections(wd,ht,size_x,size_y) {
+  var sections = [];
+  for (var i = 0; i<=wd-size_x; i+=size_x) {
+    for (var j = 0; j<=ht-size_y; j+=size_y) {
+      sections.push([i,j,size_x,size_y]);
+    }
+  }
+  
+  var extra_wd = (wd % size_x);
+  var extra_ht = (ht % size_y);
+  
+  // any overhang/remaining image, draw that too:
+  if (extra_wd) {
+    var done = Math.floor(wd / size_x) * size_x;
+    for (var j = 0; j<=ht-size_y; j += size_y) {
+      sections.push([done,j,extra_wd,size_y]);
+    }
+  }
+  if (extra_ht) {
+    var done = Math.floor(ht / size_y) * size_y;
+    for (var i = 0; i<=wd-size_x; i += size_x) {
+      sections.push([i,done,size_x,extra_ht]);
+    }
+  }
+  if (extra_ht && extra_wd) {
+    var done_x = Math.floor(wd / size_x) * size_x;
+    var done_y = Math.floor(ht / size_y) * size_y;
+    sections.push([done_x,done_y,extra_wd,extra_ht]);
+  }
+  
+  return sections;
+}
+
+/** Controls sending of the parallel sections of the image
+ *    Initial number to send
+ *    Sending the initial load
+ *    Sending another section when one is received
+ */
+var sendingSections = {
+  maxAtOnce: 10,
+  currentNum: 0,
+  missed: [],
+  sections: [],
+  data: '',
+  sendSections: function(data, sections) {
+    this.data = jQuery.extend(true,{}, data);
+    this.sections = sections;
+    while (this.currentNum<this.maxAtOnce) {
+      send(this.data, this.sections.shift());
+      this.currentNum++;
+    }
+  },
+  responseReceived: function() {
+    if (this.sections.length>0) {
+      send(this.data, this.sections.shift());
+    }
+    else if (this.missed.length>0) { // any errors/timeouts, redo
+      send(this.data, this.missed.shift());
+    }
+    else {
+      this.currentNum--;
+    }
+  },
+  addSectionMissed: function(section) {
+    this.missed.push(section);
+  }
+}
+  
+/** AJAX request for sending a section to be processed
+ *    Configures CORS to allow this to be done
+ *    Address of Lambda functions is hard-coded here
+ *    Upon reply, updates the image displayed -- updateImage()
+ */
+function send(data, section) {
+    var path = 'https://a98j1w4jae.execute-api.us-west-2.amazonaws.com/prod/q40iln';
+    var toSend = {
+      'section':     section,
+      'lightSource': data.lightSource,
+      'cameraPos':   data.cameraPos,
+      'objects':     data.objects,
+    }
+    $.ajax({
+        type: 'POST',
+        url: path,
+        data: JSON.stringify(toSend),
+        success: function(res) {
+            if (!res.error) {
+              console.log('POST success');
+              // console.log(JSON.stringify(res));
+              // var c = getCanvas(9,9);
+              // drawPixels(c, res);
+              updateImage(res);
+            }
+            else {
+              console.log('POST error');
+            }
+        },
+        error: function() {
+          sendingSections.addSectionMissed(section);
+        },
+        complete: function() {
+          sendingSections.responseReceived();
+        },
+        dataType: 'json', 
+        timeout: 120000,
+        contentType: 'application/json; charset=UTF-8',
+        crossDomain: true,  // Needed for CORS
+    });
+}
+
+
+/* Below are functions used for testing */
+ 
+/* Saves the pixels received in a text file */
 function savePixelsReceived(filename) {
   var blob = new Blob(pixelss, {type: "text/plain;charset=utf-8"});
   saveAs(blob, filename)
 }
 
+/* Initiates a test of drawing squares to full the canvas */
 var offlineLoop = '';
-var testFullImage = function(canvas, wd, ht) {
+var testFullCanvas = function(canvas, wd, ht) {
   var i = 0; 
   offlineLoop = setInterval(function(s) {
     if (i>=wd) {
@@ -53,6 +232,7 @@ var testFullImage = function(canvas, wd, ht) {
   }, 250);
 }
 
+/* Used by canvas draw test to fill a segment of the canvas */
 function drawSquare(canvas,beg_x,beg_y,end_x,end_y,col) {
   for (var i = beg_x; i<end_x; i++) {
     for (var j = beg_y; j<end_y; j++) {
@@ -64,6 +244,7 @@ function drawSquare(canvas,beg_x,beg_y,end_x,end_y,col) {
   console.log('drew square '+beg_x+' '+beg_y+' '+end_x+' '+end_y);
 }
 
+/* A populated rendering data object */
 var testImage = {
   'section':[],
   'lightSource': {'x':-10,'y':0,'z':0},
@@ -100,130 +281,4 @@ var testImage = {
       'colour': {'x':255,'y':255,'z':255}
     }
   ],
-}
-
-$(document).ready(function() {
-  // var width = 400;
-  // var height = 400;
-  // var div = 30;
-  // canvas = getCanvas(width,height);
-  
-  // // testFullImage(canvas, width, height);
-  
-  // var s = getSections(width,height,div,div);
-  // for (var i = 0; i< 100; i++) s.shift(); // skipping ahead for now
-  // sendingSections.sendSections(testImage,s);
-});
-
-function startCanvasRender() {
-  var width = 400;
-  var height = 400;
-  var div = 30;
-  canvas = getCanvas(width,height);
-  
-  // testFullImage(canvas, width, height);
-  
-  var s = getSections(width,height,div,div);
-  // for (var i = 0; i< 100; i++) s.shift(); // skipping ahead for now
-  sendingSections.sendSections(testImage,s);
-}
-
-var numDraws = 0;
-function updateImage(data) {
-  numDraws++;
-  drawPixels(canvas, data);
-}
-
-function getSections(wd,ht,size_x,size_y) {
-  var sections = [];
-  for (var i = 0; i<=wd-size_x; i+=size_x) {
-    for (var j = 0; j<=ht-size_y; j+=size_y) {
-      sections.push([i,j,size_x,size_y]);
-    }
-  }
-  
-  var extra_wd = (wd % size_x);
-  var extra_ht = (ht % size_y);
-  
-  // any overhang/remaining image, draw that too:
-  if (extra_wd) {
-    var done = Math.floor(wd / size_x) * size_x;
-    for (var j = 0; j<=ht-size_y; j += size_y) {
-      sections.push([done,j,extra_wd,size_y]);
-    }
-  }
-  if (extra_ht) {
-    var done = Math.floor(ht / size_y) * size_y;
-    for (var i = 0; i<=wd-size_x; i += size_x) {
-      sections.push([i,done,size_x,extra_ht]);
-    }
-  }
-  if (extra_ht && extra_wd) {
-    var done_x = Math.floor(wd / size_x) * size_x;
-    var done_y = Math.floor(ht / size_y) * size_y;
-    sections.push([done_x,done_y,extra_wd,extra_ht]);
-  }
-  
-  return sections;
-}
-
-var sendingSections = {
-  maxAtOnce: 10,
-  currentNum: 0,
-  missed: [],
-  sections: [],
-  sendSections: function(data, sections) {
-    this.sections = sections;
-    while (this.currentNum<this.maxAtOnce) {
-      data.section = this.sections.shift();
-      send(data);
-      this.currentNum++;
-    }
-  },
-  responseReceived: function() {
-    if (this.sections.length>0) {
-      // var idx = Math.floor(Math.random() * this.sections.length);
-      // var data = testImage;
-      testImage.section = this.sections.shift();
-      send(testImage);
-    }
-    else {
-      this.currentNum--;
-    }
-  },
-  addSectionMissed: function(section) {
-    this.missed.push(section);
-  }
-}
-  
-function send(data) {
-    var path = 'https://a98j1w4jae.execute-api.us-west-2.amazonaws.com/prod/q40iln';
-    // console.log(JSON.stringify(data));
-    $.ajax({
-        type: 'POST',
-        url: path,
-        data: JSON.stringify(data),
-        success: function(res) {
-            if (!res.error) {
-              console.log('POST success');
-              // console.log(JSON.stringify(res));
-              // var c = getCanvas(9,9);
-              // drawPixels(c, res);
-              updateImage(res);
-            }
-            else {
-              console.log('POST error');
-            }
-        },
-        error: function() {
-          addSectionMissed(section);
-        },
-        complete: function() {
-          sendingSections.responseReceived();
-        },
-        dataType: 'json', 
-        timeout: 120000,
-        contentType: 'application/json; charset=UTF-8',
-        crossDomain: true,
-    });
 }
